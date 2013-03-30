453ac75c3392a2855b23664ecc769b9dbeca2375
7chapters <jayram.rout@7chapters.info>
2013-03-30 12:04:50 -0700

    Update

---
+++ HibernateMappings/src/main/java/info/_7chapters/hibernate/util/SchemaExportUtil.java
@@ -0,0 +1,17 @@
+package info._7chapters.hibernate.util;
+ 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+ 
+public class SchemaExportUtil {
+ 
+	public static void main(String arg[]){
+		Configuration config=new Configuration();
+		config.configure("hibernate.mysql.cfg.xml");
+		new SchemaExport(config).create(true, true);
+		SessionFactory factory=config.buildSessionFactory();
+		Session sess=factory.openSession();
+	}
+}
