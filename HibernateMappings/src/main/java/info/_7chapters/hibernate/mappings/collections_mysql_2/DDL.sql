f26e6a889628bc00854da7904e3726f18586e1d6
7chapters <jayram.rout@7chapters.info>
2013-04-24 21:06:13 -0700

    Update

--- HibernateMappings/src/main/java/info/_7chapters/hibernate/mappings/collections_mysql_2/DDL.sql
+++ HibernateMappings/src/main/java/info/_7chapters/hibernate/mappings/collections_mysql_2/DDL.sql
@@ -1,10 +1,12 @@
-create table EMPLOYEE ( id INT NOT NULL auto_increment, 
-		first_name VARCHAR(20) default NULL, 
-		last_name VARCHAR(20) default NULL, 
-		salary INT default NULL, 
-		PRIMARY KEY (id) );
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-create table CERTIFICATE ( id INT NOT NULL auto_increment, 
-	certificate_name VARCHAR(30) default NULL, 
-	employee_id INT default NULL, 
-	PRIMARY KEY (id) );
+create table `CERTIFICATE` ( id INT NOT NULL auto_increment, 
+	`certificate_name` VARCHAR(30) default NULL, 
+	`employee_id` INT default NULL, 
+	PRIMARY KEY (`id`) );
